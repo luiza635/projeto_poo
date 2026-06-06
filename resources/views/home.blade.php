4936d9ff9f32f0e61f29b4e32e4bd06f688124a4
Luiza <mariaalves1954@aluno.ce.gov.br>
2026-06-06 16:10:53 -0300

    inicio do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ola luiza</h1>
+    <a href="/jornal">Clique aqui para ver o jornal</a>
+</body>
+</html>
