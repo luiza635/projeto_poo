6dba3cbbfa96fc1a0bbf1fadbdd63bb51a6ac812
Luiza <mariaalves1954@aluno.ce.gov.br>
2026-06-16 10:53:53 -0300

    login ok

--- routes/web.php
+++ routes/web.php
@@ -7,22 +7,27 @@ use App\Http\Controllers\JournalistController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return Auth::check()
-        ? redirect('/jornalista')
-        : redirect('/login');
+        ? redirect()->route('jornalista.dashboard')
+        : redirect()->route('login');
 });
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-
-Route::get('/jornalista', [JournalistController::class, 'dashboard'])
-    ->middleware('auth');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/jornalista', [JournalistController::class, 'dashboard'])
+        ->name('jornalista.dashboard');
+
     Route::resource('articles', ArticleController::class);
     Route::resource('categories', CategoryController::class);
     Route::resource('gallery', GalleryController::class);
+
+    // ADMIN
+    Route::get('/admin', [AdminController::class, 'index'])
+        ->name('admin.dashboard');
+
 });
+
+require __DIR__.'/auth.php';
